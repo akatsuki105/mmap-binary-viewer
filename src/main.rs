61c7ae904702104271be62dc46f2a57689bc80dc
pokemium <bluejapan73+dev@gmail.com>
2022-10-17 07:01:57 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
+use std::fs::File;
+
 use egui::Vec2;
 use egui_memory_editor::{option_data::MemoryEditorOptions, MemoryEditor};
+use memmap::Mmap;
 
 fn main() {
+    let file = File::open("/tmp/gsnes.wram").unwrap();
+    let mmap = unsafe { Mmap::map(&file).unwrap() };
+
     let mut options = eframe::NativeOptions::default();
     options.initial_window_size = Some(Vec2::new(932., 600.));
 
     eframe::run_native(
         "Mmap Viewer",
         options,
-        Box::new(|_cc| Box::new(App::default())),
+        Box::new(|_cc| Box::new(App::new(mmap))),
     );
 }
 
 struct App {
     mem_editor: MemoryEditor,
     options: MemoryEditorOptions,
-    buf: Vec<u8>,
+    buf: Mmap,
 }
 
-impl Default for App {
-    fn default() -> Self {
+impl App {
+    fn new(mmap: Mmap) -> Self {
         let mut options = MemoryEditorOptions::default();
         options.column_count = 32;
         options.is_options_collapsed = true;
 
+        let size = mmap.len();
+
         let mem_editor = MemoryEditor::new()
             .with_options(options.clone())
-            .with_address_range("All", 0..0xFFFF)
-            .with_address_range("IO", 0xFF00..0xFF80);
+            .with_address_range("All", 0..size);
 
         Self {
             mem_editor: mem_editor,
             options: options,
-            buf: vec![0 as u8; 0x10000],
+            buf: mmap,
         }
     }
 }
