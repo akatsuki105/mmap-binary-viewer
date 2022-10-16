60f11377cf3dc196f99e975609b4c56a6abed70d
pokemium <bluejapan73+dev@gmail.com>
2022-10-17 05:51:26 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,42 @@
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use egui_memory_editor::MemoryEditor;
 
 fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Mmap Viewer",
+        options,
+        Box::new(|_cc| Box::new(App::default())),
+    );
+}
 
-    {
-        let gl_attr = video_subsystem.gl_attr();
-        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-        gl_attr.set_context_version(3, 1);
-        let (major, minor) = gl_attr.context_version();
-        println!("OK: init OpenGL: version={}.{}", major, minor);
+struct App {
+    mem_editor: MemoryEditor,
+    buf: Vec<u8>,
+    is_open: bool,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        let mem_editor = MemoryEditor::new()
+            .with_address_range("All", 0..0xFFFF)
+            .with_address_range("IO", 0xFF00..0xFF80);
+        Self {
+            mem_editor: mem_editor,
+            buf: vec![0 as u8; 0x10000],
+            is_open: true,
+        }
     }
+}
 
-    let window = video_subsystem
-        .window("Window", 800, 600)
-        .opengl()
-        .build()
-        .unwrap();
-
-    let _gl_context = window.gl_create_context().unwrap();
-    gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as _);
-
-    // init imgui
-    let mut imgui_context = imgui::Context::create();
-    imgui_context.set_ini_filename(None);
-
-    // init imgui sdl2
-    let mut imgui_sdl2_context = imgui_sdl2::ImguiSdl2::new(&mut imgui_context, &window);
-    let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui_context, |s| {
-        video_subsystem.gl_get_proc_address(s) as _
-    });
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    'mainloop: loop {
-        for event in event_pump.poll_iter() {
-            imgui_sdl2_context.handle_event(&mut imgui_context, &event);
-            if imgui_sdl2_context.ignore_event(&event) {
-                continue;
-            }
-
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Option::Some(Keycode::Escape),
-                    ..
-                } => break 'mainloop,
-                _ => {}
-            }
-        }
-
-        imgui_sdl2_context.prepare_frame(
-            imgui_context.io_mut(),
-            &window,
-            &event_pump.mouse_state(),
-        );
-
-        let ui = imgui_context.frame();
-
-        unsafe {
-            gl::ClearColor(0.2, 0.2, 0.2, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-
-        imgui_sdl2_context.prepare_render(&ui, &window);
-        renderer.render(ui);
-
-        window.gl_swap_window();
-
-        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.mem_editor.window_ui_read_only(
+                ctx,
+                &mut self.is_open,
+                &mut self.buf,
+                |mem, address| mem[address].into(),
+            );
+        });
     }
 }
